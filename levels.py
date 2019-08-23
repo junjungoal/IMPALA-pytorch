23c9e21721dcec75514375fe31e89bb7ff71b415
Jun yamada <junyamada107@gmail.com>
2019-08-23 09:29:50 +0900

    comment out some levels

--- levels.py
+++ levels.py
@@ -1,7 +1,7 @@
 LEVELS = [
-        'contributed/dmlab30/rooms_watermaze',
-        'contributed/dmlab30/rooms_keys_doors_puzzle',
-        'contributed/dmlab30/lasertag_three_opponents_small',
-        'contributed/dmlab30/explore_goal_locations_small',
+        #'contributed/dmlab30/rooms_watermaze',
+        #'contributed/dmlab30/rooms_keys_doors_puzzle',
+        #'contributed/dmlab30/lasertag_three_opponents_small',
+        #'contributed/dmlab30/explore_goal_locations_small',
         'seekavoid_arena_01'
         ]
